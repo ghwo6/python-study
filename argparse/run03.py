9aa2bb03911926478931b0745e818beb384594cc
Hojae <ghwo6@naver.com>
2026-09-18 12:01:25 +0900

    argparse 예제 심화적으로 진행함

--- argparse/run03.py
+++ argparse/run03.py
@@ -3,10 +3,10 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument(dest="dst",action="store")
-parser.add_argument("--qp",dest="qp",action="store")
-parser.add_argument("--configure",dest="configure",action="store")
+parser.add_argument("--qp",dest="qp",action="store",default=1.3)
+parser.add_argument("--configure",dest="configure",action="store",default=3)
 args = parser.parse_args()
 
-print(f"args.dst = {args.dst}")
-print(f"args.qp = {args.qp}")
-print(f"args.configure = {args.configure}")
+print(f"args.dst = {args.dst}",f"type(args.dst) = {type(args.dst)}")
+print(f"args.qp = {args.qp}",f"type(args.qp) ={type(args.qp)}")
+print(f"args.configure = {args.configure}",f"type(args.qp) ={type(args.configure)}")
