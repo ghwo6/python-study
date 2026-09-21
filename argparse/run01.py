211fdc4438e7d04407a6e6784273cdc6a80dcb3c
김호재 <ghwo61351@c6r3s6.codyssey.kr>
2026-09-21 20:48:05 +0900

    argparse에서 parser.add_argument('--slow',help='')로 해도 문제가 없음을 확인함 dest='' 설정하지 않아도 됨

--- argparse/run01.py
+++ argparse/run01.py
@@ -4,8 +4,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-d","--decimal",dest="decimal",action="store")
 parser.add_argument("-f","--fast",dest="fast",action="store_true")
+parser.add_argument("--slow",help="")
 
 args = parser.parse_args()
 
 print(args.decimal)
 print(args.fast)
+print(args.slow)
